ed6c00d3b107cec4392408e92cf3ce8480ca80f2
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2022-11-26 08:01:27 +0700

    JDBC with SQL Server

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -42,18 +42,6 @@ public class DesignTacoController {
 	
 	@GetMapping
 	public String showDesignForm(Model model) {
-//		List<Ingredient> ingredients = Arrays.asList(
-//				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-//				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-//				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-//				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-//				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-//				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-//				new Ingredient("CHED", "Cheddar", Type.CHEESE),
-//				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-//				new Ingredient("SLSA", "Salsa", Type.SAUCE),
-//				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-//				);
 		List<Ingredient> ingredients = new ArrayList<>();
 		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 		Type[] types = Type.values();
