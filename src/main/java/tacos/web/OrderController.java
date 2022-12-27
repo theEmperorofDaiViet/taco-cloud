77255f5ec395ba35c16c5b7105b024ac035ac956
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2022-12-28 03:06:52 +0700

    add new feature: see order history

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,9 +1,12 @@
 package tacos.web;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +47,13 @@ public class OrderController {
 		orderRepo.save(order);
 		sessionStatus.setComplete();
 		return "redirect:/";
+	}
+	
+	@GetMapping
+	public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+		List<Order> orders = orderRepo.findByUserOrderByPlacedAtDesc(user);
+		model.addAttribute("orders", orders);
+		return "orderList";
 	}
 
 }
